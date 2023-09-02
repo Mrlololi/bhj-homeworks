a3283cb85f7e8f8d28394e463cf84ed1fb5ca153
mrlololi <ivikosha@gmail.com>
2023-09-02 20:39:15 +0400

    task 1 done

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -4,7 +4,10 @@ const interval = setInterval(() => {
     if(timerCount > 0){
         timerCount -= 1;
         time.textContent = timerCount;
-    } else if (timerCount === 0){
+        return;
+    }
+    
+    if (timerCount === 0){
         alert("Вы победили в конкурсе");
         clearInterval(interval);
     }
