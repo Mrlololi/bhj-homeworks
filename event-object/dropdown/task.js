046a1e6bb6415d893b3b29bd62a50d328b3e7a49
mrlololi <ivikosha@gmail.com>
2023-09-06 22:37:35 +0400

    task 1 almost done

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,21 @@
+const dropdownList = document.querySelector(".dropdown__list");
+const dropdownRealValue = document.querySelector(".dropdown__value");
+
+document.addEventListener("click", dropdownListToggle)
+
+function dropdownListToggle(){
+    dropdownList.classList.toggle("dropdown__list_active")
+}
+
+const dropdownValue = document.querySelectorAll(".dropdown__link");
+
+for(let i = 0; i < dropdownValue.length; i++){
+    dropdownValue[i].addEventListener("click", function() {
+        changeValue(dropdownValue[i].textContent);
+    });
+}
+
+function changeValue(newValue){
+    dropdownRealValue.textContent = newValue;
+    dropdownList.classList.remove("dropdown__list_active");
+}
