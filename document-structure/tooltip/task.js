2badc6870c20d76a42c9de738d3a55f2e8804cac
mrlololi <ivikosha@gmail.com>
2023-09-18 23:19:06 +0400

    task 1 fixed

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -4,6 +4,8 @@ hasTooltip.forEach(element => {
     let newElement = document.createElement("div");
     newElement.classList.add("tooltip");
     newElement.textContent = element.title;
+    let rect = element.getBoundingClientRect();
+    newElement.style.left = rect.left + "px";
     element.insertAdjacentElement("afterend", newElement);
     element.addEventListener("click", (e) => {
         newElement.classList.toggle("tooltip_active")
