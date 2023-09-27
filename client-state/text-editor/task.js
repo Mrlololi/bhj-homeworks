6ef2b68e66af1e26982c5db7344797068ef48c6e
mrlololi <ivikosha@gmail.com>
2023-09-28 02:35:17 +0400

    task 1 fixed

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -3,6 +3,11 @@ if(localStorage.getItem("editorText")){
     editor.value = localStorage.getItem("editorText")
 }
 
+if(!localStorage.getItem("editorText")){
+    editor.value = "Сотрите меня и я больше никогда не появлюсь - Это будет на вашей совести!"
+}
+
+
 editor.addEventListener("input", () => {
     localStorage.setItem("editorText", editor.value)
 })
