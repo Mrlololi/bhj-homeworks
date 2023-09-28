2038e60c590977aec050f61054a8386723714acf
mrlololi <ivikosha@gmail.com>
2023-09-28 23:40:00 +0400

    task 3 fixed

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -3,26 +3,33 @@ let welcomeDiv = document.getElementById("welcome");
 let userIdSpan = document.getElementById("user_id");
 let signInDiv = document.getElementById("signin");
 
-signInForm.addEventListener("submit", (event) => {
-    event.preventDefault();
+if (localStorage.getItem("UserID")) {
+    signInDiv.style.display = "none";
+    userIdSpan.textContent = localStorage.getItem("UserID");
+    welcomeDiv.style.display = "block";
+} else{
+    signInForm.addEventListener("submit", (event) => {
+        event.preventDefault();
 
-    let formData = new FormData(signInForm);
+        let formData = new FormData(signInForm);
 
-    fetch('https://students.netoservices.ru/nestjs-backend/auth', {
-        method: 'POST',
-        body: formData,
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            signInDiv.style.display = "none";
-            userIdSpan.textContent = data.user_id;
-            welcomeDiv.style.display = "block";
-        } else {
-            alert("Неверные данные!");
-        }
-    })
-    .catch((error) => {
-        alert("Произошла ошибка при отправке!");
+        fetch('https://students.netoservices.ru/nestjs-backend/auth', {
+            method: 'POST',
+            body: formData,
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                localUserID = localStorage.setItem("UserID", data.user_id)
+                signInDiv.style.display = "none";
+                userIdSpan.textContent = data.user_id;
+                welcomeDiv.style.display = "block";
+            } else {
+                alert("Неверные данные!");
+            }
+        })
+        .catch((error) => {
+            alert("Произошла ошибка при отправке!");
+        });
     });
-});
+}
